6ba3362c166f2de6de076fd2abbb8a44151dd026
Kirill <202536@edu.fa.ru>
2021-11-11 02:19:22 +0300

    Реализовано основное задание + доп №1

--- server.py
+++ server.py
@@ -1,4 +1,24 @@
 import socket
+import os
+
+def get_route(req):
+    method, route = '', ''
+    try:
+        head_data = req.split('\n')[0]
+        method, route, protocol = head_data.split(' ')
+        return {
+            'method': method,
+            'route': route,
+            'data': response_data(route) if route != '/close' else 'Server closed',
+        }
+    except Exception as e:
+        return None
+
+
+def response_data(route):
+    with open(os.path.join('data', route[1:])+".html") as f:
+        return ''.join(f.readlines())
+
 
 sock = socket.socket()
 
@@ -13,19 +33,28 @@ sock.listen(5)
 
 conn, addr = sock.accept()
 print("Connected", addr)
-
 data = conn.recv(8192)
+
 msg = data.decode()
+routing = get_route(msg)
+resp = f"""HTTP/1.1 200 OK
+    Server: SelfMadeServer v0.0.1
+    Content-type: text/html
+    Connection: close
 
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
+    {routing['data']}"""
 conn.send(resp.encode())
 
+while routing is None or routing['route'] != '/close':
+    data = conn.recv(8192)
+    msg = data.decode()
+    routing = get_route(msg)
+
+    resp = f"""HTTP/1.1 200 OK
+    Server: SelfMadeServer v0.0.1
+    Content-type: text/html
+    Connection: close
+    
+    {routing['data']}"""
+    conn.send(resp.encode())
 conn.close()
