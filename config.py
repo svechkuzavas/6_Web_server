f761d8ec2d63ed517dc71cb0fa7e6e953ab0bd47
Kirill <202536@edu.fa.ru>
2021-11-25 18:41:25 +0300

    допы для практикума

---
+++ config.py
@@ -0,0 +1,3 @@
+PORT = 8080
+MAX_BYTES = 8192
+PATH = 'data'
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import os
 import socket
 import datetime
+import config
+import threading
+import logging
+import base64
 
 
 class HTTPServer:
@@ -14,21 +18,22 @@ class HTTPServer:
         try:
             serv_sock.bind(('', self._port))
             serv_sock.listen(1)
-            print(f"Server is started and listening on port {self._port}")
+            logging.info(f"Server is started and listening on port {self._port}")
             while True:
                 conn, addr = serv_sock.accept()
-                print(f"New connection: {str(addr)}")
+                logging.info(f"New connection: {str(addr[0])}:{str(addr[1])}")
                 try:
-                    self.handle_client(conn)
+                    t = threading.Thread(target=self.handle_client, args=(conn,))
+                    t.start()
                 except Exception as e:
-                    print('Client handling failed', e)
+                    logging.info(f'Client handling failed {addr}', e)
         finally:
             serv_sock.close()
 
     def handle_client(self, conn):
         try:
             req = self.parse_request(conn)
-            self.send_response(conn, req['data'])
+            self.send_response(req['status_code'], conn, req['data'], req['message'])
         except ConnectionResetError:
             conn = None
         except Exception as e:
@@ -39,19 +44,50 @@ class HTTPServer:
     def parse_request(self, conn):
         rfile = conn.makefile('r')
         raw = rfile.readline(64*1024 + 1).split()
-        filename = '/index' if raw[1] == '/' else raw[1]
-        return {
+        status_code = 200
+        filename = '/index.html' if raw[1] == '/' else raw[1]
+        response_obj = {
+            'status_code': 200,
             'method': raw[0],
             'route': raw[1],
-            'data': self.read_file(filename) if raw[1] != '/close' else 'Server closed',
+            'data': '',
+            'message': 'OK'
         }
+        if filename.split('.')[1] in ['jpg', 'jpeg', 'png']:
+            try:
+                with open(os.path.join(config.PATH, filename[1:]), 'rb') as f:
+                    response_obj['data'] = f"""<html><head></head><body><img src="data:image/png;base64,{base64.b64encode(f.read()).decode('utf-8')}"/></body></html>"""
+            except Exception as e:
+                response_obj['status_code'] = 404
+                response_obj['data'] = self.read_file('/404.html')
+                response_obj['message'] = 'NotFound'
+                return response_obj
+        else:
+            try:
+                data = self.read_file(filename) if raw[1] != '/close' else 'Server closed'
+                response_obj['data'] = data
+            except Exception as e:
+                response_obj['status_code'] = 404
+                response_obj['data'] = self.read_file('/404.html')
+                response_obj['message'] = 'NotFound'
+                logging.info(f"{' '.join(raw)} -> HTTP/1.1 {response_obj['status_code']} {response_obj['message']}")
+                return response_obj
+        if not filename.split('.')[1] in ['js', 'html', 'css', 'jpg', 'png', 'jpeg']:
+            response_obj['status_code'] = 403
+            response_obj['data'] = self.read_file('/403.html')
+            response_obj['message'] = 'Forbidden'
+            logging.info(f"{' '.join(raw)} -> HTTP/1.1 {response_obj['status_code']} {response_obj['message']}")
+            return response_obj
+
+        logging.info(f"{' '.join(raw)} -> HTTP/1.1 {response_obj['status_code']} {response_obj['message']}")
+        return response_obj
 
     def read_file(self, route):
-        with open(os.path.join('data', f'{route[1:]}.html')) as f:
+        with open(os.path.join(config.PATH, f'{route[1:]}')) as f:
             return ''.join(f.readlines())
 
-    def send_response(self, conn, resp_body):
-        response = f"""HTTP/1.1 200 OK
+    def send_response(self, status_code, conn, resp_body, message):
+        response = f"""HTTP/1.1 {status_code} {message}
 Server: KirillZaycevWebServer v0.0.1
 Content-type: text/html
 Connection: close
@@ -67,5 +103,6 @@ Content-length: {len(resp_body)}
 
 
 if __name__ == '__main__':
-    server = HTTPServer(8080)
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG, filename='server.log', filemode='a')
+    server = HTTPServer(config.PORT)
     server.run()
